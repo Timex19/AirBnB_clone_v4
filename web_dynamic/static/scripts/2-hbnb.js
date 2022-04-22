e5fc2f01bbcaa3b5ed53c8744bb6c68b04bdf62d
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-22 21:09:58 +0100

    Filter places by Amenity and modificated with the dom and make the page separate

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(init);
+
+function init () {
+  const amenityObj = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObj[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenityObj);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+
+}
